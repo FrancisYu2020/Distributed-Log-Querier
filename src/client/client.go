a9de202423b02f4e840ddeeec8b8f60f9d40ba74
Tian23 <tian23@illinois.edu>
2022-09-05 22:12:00 -0500

    Add handle error feature

--- src/client/client.go
+++ src/client/client.go
@@ -20,29 +20,36 @@ func checkFileIsExist(filename string) bool {
 	return true
 }
 
+func handleError(err error, c chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	c <- string(err.Error())
+}
+
 func main() {
-	var wg sync.WaitGroup
+	var wg sync.WaitGroup // use wait group to keep synchronization
 	defer wg.Wait()
 
-	c := make(chan string)
+	c := make(chan string) // use chanel to send logs safely
 	ips := [3]ipAddress{{"172.22.156.72:1234", "machine 1"}, {"172.22.158.72:1234", "machine 2"}, {"172.22.94.72:1234", "machine 3"}}
 
 	for _, ip := range ips {
-		wg.Add(1)
-		go func(ip ipAddress) {
-			client, err := rpc.Dial("tcp", ip.address)
+		wg.Add(1)               // add one when set a new task
+		go func(ip ipAddress) { // connect to one server and try to execute RPC on that server
+			client, err := rpc.Dial("tcp", ip.address) // set connection
 			if err != nil {
-				log.Fatal(err)
+				handleError(err, c, &wg)
+				return
 			}
 
 			var reply string
-			err = client.Call("grepLogService.GrepLog", "grep -E log ../test_logs/log1", &reply)
+			err = client.Call("grepLogService.GrepLog", "grep -E log ../test_logs/log1", &reply) // RPC
 			if err != nil {
-				log.Fatal(err)
+				handleError(err, c, &wg)
+				return
 			}
-			c <- reply
+			c <- reply // use channel send logs back
 
-			wg.Done()
+			wg.Done() // minus one when finish a task
 		}(ip)
 	}
 
