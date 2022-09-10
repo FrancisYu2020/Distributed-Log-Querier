9ea66342d0e9fdd2b277d8a050a3f3528b65eef3
hangy6 <hangy6@fa22-cs425-2210.cs.illinois.edu>
2022-09-10 05:26:03 -0500

    new commit

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -3,7 +3,7 @@ git add .
 git commit -m "new commit"
 git push origin hangy6
 cd src/scripts/
-# bash update_source_code.sh
+bash update_source_code.sh
 
 # test 2 or 3 machines on these and expand
 bash build_all_server_client.sh
