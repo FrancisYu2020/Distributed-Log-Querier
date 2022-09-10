8ea3cfd9850970331b2d3a29f09420600ab446ce
hangy6 <hangy6@fa22-cs425-2210.cs.illinois.edu>
2022-09-10 04:33:22 -0500

    test

--- src/scripts/update_source_code.sh
+++ src/scripts/update_source_code.sh
@@ -3,11 +3,13 @@
 for val in {1..9}
 do
     echo VM$val Updating
-    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "cd ./mp1-hangy6-tian23; git fetch --all; git reset --hard origin/main; git pull; exit"
+    ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "cd ./mp1-hangy6-tian23; git checkout hangy6; git pull; exit"
+    # ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "cd ./mp1-hangy6-tian23; git checkout hangy6; git fetch --all; git reset --hard origin/main; git pull; exit"
     echo VM$val Updated
 done
 echo VM10 Updating
-ssh tian23@fa22-cs425-2210.cs.illinois.edu "cd ./mp1-hangy6-tian23; git fetch --all; git reset --hard origin/main; git pull; exit"
+ssh hangy6@fa22-cs425-2210.cs.illinois.edu "cd ./mp1-hangy6-tian23; git checkout hangy6; git pull; exit"
+# ssh hangy6@fa22-cs425-2210.cs.illinois.edu "cd ./mp1-hangy6-tian23; git checkout hangy6; git fetch --all; git reset --hard origin/main; git pull; exit"
 echo VM10 Updated
 
 echo "All VMs Have Been Updated!"
