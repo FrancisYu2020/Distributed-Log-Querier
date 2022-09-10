5e1d3d79202f6ea74bddcd4f1a8e6519ec251bac
hangy6 <hangy6@fa22-cs425-2210.cs.illinois.edu>
2022-09-10 06:49:00 -0500

    add gitignore

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -4,5 +4,5 @@ cd ~/mp1-hangy6-tian23
 # export GOPATH=`pwd`
 cd ~/mp1-hangy6-tian23/src/
 go build -o ../../bin/client ./client_main.go
-cd ~/mp1-hangy6-tian23/src/server
+cd ~/mp1-hangy6-tian23/src/
 go build -o ../../bin/server ./server_main.go
--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -1,11 +1,11 @@
-cd /home/hangy6/mp1-hangy6-tian23
-git add .
-git commit -m "new commit"
-git push origin hangy6
-cd src/scripts/
-bash update_source_code.sh
+# cd /home/hangy6/mp1-hangy6-tian23
+# git add .
+# git commit -m "new commit"
+# git push origin hangy6
+# cd src/scripts/
+# bash update_source_code.sh
 
 # test 2 or 3 machines on these and expand
-bash build_all_server_client.sh
-bash kill_server.sh
-bash run_all_server.sh
+bash /home/hangy6/mp1-hangy6-tian23/src/scripts/build_all_server_client.sh
+bash /home/hangy6/mp1-hangy6-tian23/src/scripts/kill_server.sh
+bash /home/hangy6/mp1-hangy6-tian23/src/scripts/run_all_server.sh
