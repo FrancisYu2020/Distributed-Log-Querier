9c4e88bf94b6be9540f050dde712f089f19d0ad1
hangy6 <hangy6@illinois.edu>
2022-09-10 07:08:51 -0500

    hhh

--- src/scripts/download_code.sh
+++ src/scripts/download_code.sh
@@ -3,7 +3,7 @@
 for val in {1..3}
 do
     echo VM$val Updating
-    ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "git clone git@gitlab.engr.illinois.edu:hangy6/mp1-hangy6-tian23.git; git checkout hangy6; git branch; exit"
+    ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "git clone git@gitlab.engr.illinois.edu:hangy6/mp1-hangy6-tian23.git; git checkout hangy6; git branch; git config --global user.name 'hangy6'; git config --global user.email hangy6@illinois.edu; exit"
     echo VM$val Updated
 done
 
