f4dca4d8f5811f17f2d0d2924094c318701215f1
hangy6 <hangy6@fa22-cs425-2210.cs.illinois.edu>
2022-09-10 05:24:45 -0500

    new commit

--- src/scripts/run_all_server.sh
+++ src/scripts/run_all_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for val in {1..2}
+for val in {1..9}
 do
     echo VM$val Starting
     ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "cd /home/hangy6/mp1-hangy6-tian23/bin/; nohup ./server>/dev/null 2>&1&"
