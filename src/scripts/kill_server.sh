3eb8168075cb7084e1ceb5101be4c202319141ef
hangy6 <hangy6@fa22-cs425-2210.cs.illinois.edu>
2022-09-10 03:34:35 -0500

    new commit

--- src/scripts/kill_server.sh
+++ src/scripts/kill_server.sh
@@ -3,11 +3,11 @@
 for val in {1..2}
 do
     echo Killing VM$val Server
-    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "cd /home/tian23/mp1-hangy6-tian23/bin/; pkill server"
+    ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "cd /home/hangy6/mp1-hangy6-tian23/bin/; pkill server"
     echo VM$val Done
 done
 echo Killing VM10 Server
-ssh tian23@fa22-cs425-2210.cs.illinois.edu "cd /home/tian23/mp1-hangy6-tian23/bin/; pkill server"
+ssh hangy6@fa22-cs425-2210.cs.illinois.edu "cd /home/hangy6/mp1-hangy6-tian23/bin/; pkill server"
 echo VM10 Done
 
 echo "All Servers Shut Down!"
